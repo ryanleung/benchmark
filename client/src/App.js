78694121d2905c4ab3852d14f3364c0ef8d3e8ca
Ryan Leung <ryanleung@berkeley.edu>
2017-02-10 23:31:20 -0800

    Install Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+ 
+// Needed for onTouchTap 
+// http://stackoverflow.com/a/34015469/988941 
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
