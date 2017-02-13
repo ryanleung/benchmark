a91674ccc497359ea745768640e334a8967a2dbe
Ryan Leung <ryanleung@berkeley.edu>
2017-02-13 00:37:16 -0800

    Add google fonts

--- client/src/components/AddBenchmarkPage.js
+++ client/src/components/AddBenchmarkPage.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 import NavBar from './NavBar.js';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import AddBenchmarkMuiTheme from './AddBenchmarkMuiTheme.js';
 
 
 class AddBenchmarkPage extends Component {
   render() {
     return (
       <div className="AddBenchmarkPage">
-        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <MuiThemeProvider muiTheme={AddBenchmarkMuiTheme}>
           <NavBar />
         </MuiThemeProvider>
       </div>
