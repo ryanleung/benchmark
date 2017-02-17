e4e4d112c41091b6f83bbc0353a505edcfc713e6
Ryan Leung <ryanleung@berkeley.edu>
2017-02-17 17:06:01 -0500

    Skeleton for sign in page plus some minor css spacing cleanup

---
+++ client/src/components/SignInPage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import {Card, CardText} from 'material-ui/Card';
+import Divider from 'material-ui/Divider';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+class SignInPage extends Component {
+ render() {
+  return (
+    <div>
+    </div>
+  )
+ }
+}
