e046e52fbe7d675e08a70bee379ac206d13071e1
Ryan Leung <ryanleung@berkeley.edu>
2017-02-21 22:14:43 -0500

    Add login page as a modal activated on navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,30 +1,52 @@
 import React, {Component} from 'react';
 
 import AppBar from 'material-ui/AppBar';
+import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 
+import SignInPage from './SignInPage.js';
 
 class NavBar extends Component {
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
 
   handleTitleTap = () => {
   };
 
   handleLoginTap = () => {
+    this.handleOpen();
   };
 
   render() {
     return (
-      <AppBar 
-        zDepth={0}
-        title="Kimono Metrics"
-        showMenuIconButton={false}
-        onTitleTouchTap={this.handleTitleTap}
-        iconElementRight={
-          <FlatButton
-          label="Sign In"
-          onTouchTap={this.handleLoginTap} />
-        }
-      />
+      <div>
+        <AppBar 
+          zDepth={0}
+          title="Kimono Metrics"
+          showMenuIconButton={false}
+          onTitleTouchTap={this.handleTitleTap}
+          iconElementRight={
+            <FlatButton
+            label="Sign In"
+            onTouchTap={this.handleLoginTap} />
+          }
+        />
+        <Dialog 
+          open={this.state.open}
+          onRequestClose={this.handleClose}>
+          <SignInPage />
+        </Dialog>
+      </div>
     )
   }
 }
