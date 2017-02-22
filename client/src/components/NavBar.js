0162b02500990469a3425eb1b60806b1d34a7034
Ryan Leung <ryanleung@berkeley.edu>
2017-02-21 21:45:13 -0500

    Add login button to nav

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,31 @@
 import React, {Component} from 'react';
+
 import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
 
 
 class NavBar extends Component {
+
+  handleTitleTap = () => {
+  };
+
+  handleLoginTap = () => {
+  };
+
   render() {
     return (
       <AppBar 
         zDepth={0}
         title="Kimono Metrics"
-        showMenuIconButton={false} />
-        )
+        showMenuIconButton={false}
+        onTitleTouchTap={this.handleTitleTap}
+        iconElementRight={
+          <FlatButton
+          label="Sign In"
+          onTouchTap={this.handleLoginTap} />
+        }
+      />
+    )
   }
 }
 
